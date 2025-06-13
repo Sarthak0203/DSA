88c2983d8a8ac5b4c955e301b66234465ece296b
Sarthak Chaudhary <138149235+Sarthak0203@users.noreply.github.com>
2025-06-13 10:16:22 +0530

    Time: 40 ms (27.05%) | Memory: 27.7 MB (14.68%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,14 +1,19 @@
 class Solution {
-    int memoization(int i,int j,string& text1,string& text2,vector<vector<int>>& dp){
+
+private:
+    int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp){
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(text1[i]==text2[j]) return dp[i][j]=1+memoization(i-1,j-1,text1,text2,dp);
-        else return dp[i][j]=max(memoization(i-1,j,text1,text2,dp),memoization(i,j-1,text1,text2,dp));
+
+        if(s1[i]==s2[j]) return dp[i][j]=1+f(i-1, j-1, s1, s2, dp);
+        else return dp[i][j] = max(f(i-1, j, s1, s2, dp), f(i, j-1, s1, s2, dp));
     }
+
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int m=text1.size(),n=text2.size();
+        int m=text1.length();
+        int n=text2.length();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return memoization(m-1,n-1,text1,text2,dp);
+        return f(m-1, n-1, text1, text2, dp);
     }
 };
