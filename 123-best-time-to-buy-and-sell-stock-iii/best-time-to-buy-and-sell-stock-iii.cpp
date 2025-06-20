658e9880c09970b9470dd7ef0b0e86d9a89056bc
Sarthak Chaudhary <138149235+Sarthak0203@users.noreply.github.com>
2025-06-20 11:17:25 +0530

    Time: 423 ms (35.18%) | Memory: 224.1 MB (17.44%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,32 @@
 class Solution {
-public:
-    int dp[100001][2][2];
-    int n;
-    vector<int> nums;
-    int rec(int i, int j, int k) {
-        if (k >= 2 || i >= n)
+    int helper(vector<int>& prices, int ind, int buy, int cap, int n, vector<vector<vector<int>>>& dp){
+        if(ind == n || cap == 0){
             return 0;
-        if (dp[i][j][k] != -1)
-            return dp[i][j][k];
-        int ans = 0;
-        ans = max(ans, rec(i + 1, j, k));
-        if (j == 0)
-            ans = max(ans, rec(i + 1, 1, k) - nums[i]);
-        if (j == 1)
-            ans = max(ans, rec(i + 1, 0, k + 1) + nums[i]);
-        dp[i][j][k] = ans;
-        return ans;
+        }
+        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+
+        int profit = 0;
+        if(buy == 0){
+            // We can buy
+            profit = max(
+                helper(prices, ind+1, 0, cap, n, dp),                
+                helper(prices, ind+1, 1, cap, n, dp) - prices[ind]  
+            );
+        } else {
+            // We can sell
+            profit = max(
+                helper(prices, ind+1, 1, cap, n, dp),                
+                prices[ind] + helper(prices, ind+1, 0, cap-1, n, dp) 
+            );
+        }
+
+        return dp[ind][buy][cap] = profit;
     }
+
+public:
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
-        nums = prices;
-        memset(dp, -1, sizeof(dp));
-        return rec(0, 0, 0);
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, -1)));
+        return helper(prices, 0, 0, 2, n, dp); 
     }
 };
